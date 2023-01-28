8ac727553a015ef30da5db07e66a23f6980217ca
ummagohil <umgo0510@gmail.com>
2023-01-28 17:06:38 +0000

    current code

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,15 +1,45 @@
-import { Form } from "@remix-run/react";
+import {
+	Form,
+	useActionData,
+	useLoaderData,
+	useSearchParams,
+} from "@remix-run/react";
+import {
+	ActionFunction,
+	json,
+	LoaderFunction,
+} from "@remix-run/server-runtime";
+import { redirect } from "@remix-run/node";
+
+export const action: ActionFunction = async ({ request }) => {
+	const formData = await request.formData();
+	const Habit = formData.get("habit");
+	console.log("habit", Habit);
+	redirect("/");
+	return Habit;
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+	const url = new URL(request.url);
+	//const search = new URLSearchParams(url.search);
+	//console.log(url);
+	const habit = url.searchParams.getAll("habit");
+	return json(habit);
+};
 
 export default function Index() {
+	const data = useLoaderData<typeof loader>();
+	console.log(data);
 	return (
 		<div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-			<Form method="post">
-				<input type="text" />
+			<Form replace method="post">
+				<input name="habit" type="text" />
 				<button type="submit">Add Habit</button>
 			</Form>
 			<p>
 				SHOW HABIT <button type="submit">delete habit</button>
 			</p>
+			{data}
 		</div>
 	);
 }
