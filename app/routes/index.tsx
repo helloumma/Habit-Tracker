7b58e52332955dd9ea9dcce803aea4fb82f5f880
ummagohil <umgo0510@gmail.com>
2023-03-22 12:27:38 +0000

    current code

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,45 +1,46 @@
 import {
-	Form,
-	useActionData,
-	useLoaderData,
-	useSearchParams,
+  Form,
+  useActionData,
+  useLoaderData,
+  useSearchParams,
 } from "@remix-run/react";
-import {
-	ActionFunction,
-	json,
-	LoaderFunction,
-} from "@remix-run/server-runtime";
+import type { ActionFunction, LoaderFunction } from "@remix-run/server-runtime";
+import { json } from "@remix-run/server-runtime";
 import { redirect } from "@remix-run/node";
+import { prisma } from "~/db.server";
 
 export const action: ActionFunction = async ({ request }) => {
-	const formData = await request.formData();
-	const Habit = formData.get("habit");
-	console.log("habit", Habit);
-	redirect("/");
-	return Habit;
+  const formData = await request.formData();
+  const Habit = formData.get("habit");
+  //console.log("habit", Habit);
+  // redirect("/");
+  return Habit;
 };
 
 export const loader: LoaderFunction = async ({ request }) => {
-	const url = new URL(request.url);
-	//const search = new URLSearchParams(url.search);
-	//console.log(url);
-	const habit = url.searchParams.getAll("habit");
-	return json(habit);
+  const url = new URL(request.url);
+  //const search = new URLSearchParams(url.search);
+  //console.log(url);
+  const habit = url.searchParams.getAll("habit");
+  //console.log(habit);
+  return json(habit);
 };
 
 export default function Index() {
-	const data = useLoaderData<typeof loader>();
-	console.log(data);
-	return (
-		<div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-			<Form replace method="post">
-				<input name="habit" type="text" />
-				<button type="submit">Add Habit</button>
-			</Form>
-			<p>
-				SHOW HABIT <button type="submit">delete habit</button>
-			</p>
-			{data}
-		</div>
-	);
+  const data = useLoaderData<typeof loader>();
+
+  console.log(data);
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <Form replace method="post">
+        <input
+          name="habit"
+          type="text"
+          onChange={(e) => console.log(e.target.value)}
+        />
+        <button type="submit">Add Habit</button>
+      </Form>
+      {data}
+    </div>
+  );
 }
